525c419d8228d6ba95724574c38441ca6aa35a5f
Lucio <leedsi@bc.edu>
2019-02-10 10:34:32 -0500

    Add queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,40 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+User.findById('55c5de372bc3176b081143f67').then((user) => {
+  if(!user)
+    return console.log('Unable to find user');
+
+  console.log(JSON.stringify(user, undefined, 2));
+  //console.log(result);
+}, (e) => {
+  console.log(e);
+});
+
+
+// var id = 'A5c5f5399cd50488c4417e208';
+//
+// if(!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos - ', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo - ', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if(!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo By ID: ', todo);
+// }).catch((e) => console.log(e));
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
   var todo = new Todo({
